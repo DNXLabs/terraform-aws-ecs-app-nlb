c909dc709de852dc6f314628db1371402f4d4bbd
Helder Klemp <helderklemp@gmail.com>
2019-12-25 13:01:57 +1100

    fix securityGroup filter

--- sg-nodes.tf
+++ sg-nodes.tf
@@ -1,7 +1,7 @@
 data "aws_security_group" "selected" {
   filter {
     name   = "tag:Name"
-    values = ["ecs-${var.name}-nodes"
+    values = ["ecs-${var.cluster_name}-nodes"]
   }
 }
 
@@ -13,25 +13,5 @@ resource "aws_security_group_rule" "all_from_alb_to_ecs_nodes" {
   to_port                  = 0
   protocol                 = "TCP"
   security_group_id = data.aws_security_group.selected.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "all_from_ecs_nodes_to_ecs_nodes" {
-  description              = "Traffic between ECS nodes"
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  security_group_id        = "${aws_security_group.ecs_nodes.id}"
-  source_security_group_id = "${aws_security_group.ecs_nodes.id}"
-}
-
-resource "aws_security_group_rule" "all_from_ecs_nodes_world" {
-  description       = "Traffic to internet"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = "${aws_security_group.ecs_nodes.id}"
   cidr_blocks       = ["0.0.0.0/0"]
 }
