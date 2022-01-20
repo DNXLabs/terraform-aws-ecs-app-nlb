0da17df7c1a6e0f7302bf9afcb1a1c3d7f605880
Rodrigo Brasil <rodrigobbrasil@gmail.com>
2022-01-20 17:48:53 -0300

    feature: conditional internal load balancer

--- nlb.tf
+++ nlb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "default" {
-  count              = var.nlb_internal ? 1 : 0
+  count              = var.nlb ? 1 : 0
   name               = var.nlb_internal ? "ecs-${var.cluster_name}-${var.name}-internal" : "ecs-${var.cluster_name}-${var.name}"
   internal           = var.nlb_internal
   load_balancer_type = "network"
