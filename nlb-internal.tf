5b92b44b901a57e3a876d2ecac486a4d843856a1
edson.siqueira@dnx.solutions <Edson Soares Siqueira>
2022-03-17 21:37:27 -0300

    Change '-tcp' to '-int' in nlb-internal.tf

--- nlb-internal.tf
+++ nlb-internal.tf
@@ -6,7 +6,7 @@ resource "random_string" "nlb_prefix" {
 
 resource "aws_lb" "default" {
   count              = var.nlb_internal ? 1 : 0
-  name               = var.nlb_internal ? format("%s-%s-tcp", substr("${var.cluster_name}-${var.name}-internal", 0, 23), random_string.nlb_prefix.result) : format("%s-%s-tcp", substr("ecs-${var.cluster_name}-${var.name}", 0, 23), random_string.nlb_prefix.result)
+  name               = var.nlb_internal ? format("%s-%s-int", substr("${var.cluster_name}-${var.name}-internal", 0, 23), random_string.nlb_prefix.result) : format("%s-%s-int", substr("ecs-${var.cluster_name}-${var.name}", 0, 23), random_string.nlb_prefix.result)
   internal           = var.nlb_internal
   load_balancer_type = "network"
   subnets            = var.subnets
