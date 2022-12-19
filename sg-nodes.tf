bdf8ccf49278afcd59f38657d9f8ccba01a5a94d
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2022-12-19 20:07:17 +1000

    include vpc default from sg

--- sg-nodes.tf
+++ sg-nodes.tf
@@ -5,6 +5,16 @@ data "aws_security_group" "selected" {
   }
 }
 
+resource "aws_security_group_rule" "vpc_from_nlb_to_ecs_nodes" {
+  description       = "From NLB subnet"
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "TCP"
+  security_group_id = data.aws_security_group.selected.id
+  cidr_blocks       = var.nlb_subnets_cidr
+}
+
 
 resource "aws_security_group_rule" "all_from_nlb_to_ecs_nodes" {
   description       = "for NLB"
@@ -13,5 +23,5 @@ resource "aws_security_group_rule" "all_from_nlb_to_ecs_nodes" {
   to_port           = 65535
   protocol          = "TCP"
   security_group_id = data.aws_security_group.selected.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.security_group_ecs_nodes_inbound_cidrs
 }
