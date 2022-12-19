66e159d6a0bf83c52553a54b3ae3ec3dea2b69fc
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2022-12-19 20:05:59 +1000

    fix dns name

--- route53-record.tf
+++ route53-record.tf
@@ -10,5 +10,5 @@ resource "aws_route53_record" "hostname" {
   name    = var.hostname
   type    = "CNAME"
   ttl     = "300"
-  records = var.nlb_internal ? [aws_lb.default[0].dns_name] : [try(data.aws_lb.nlb_selected[0].dns_name, "")]
+  records = [aws_lb.default[0].dns_name]
 }
